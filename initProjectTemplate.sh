98734db6460f74a83eb6209d42f4afec2df12915
Mirobo <lamboris90@gmail.com>
2019-11-25 22:19:11 +0000

    correct typo and add project template init script

---
+++ initProjectTemplate.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar /workspace/libgdx/gdx-setup.jar --dir /workspace/libgdx-training-games --name libgdx-training-games --package com.apps.mirobo --mainClass Main --sdkLocation $ANDROID_HOME

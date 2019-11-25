3f90b8af4319bb0fa6b7caac916a0d3dac8409a4
Mirobo <lamboris90@gmail.com>
2019-11-25 22:50:19 +0000

    init project template

---
+++ android/src/com/apps/mirobo/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.apps.mirobo;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.apps.mirobo.Main;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Main(), config);
+	}
+}
---
+++ desktop/src/com/apps/mirobo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.apps.mirobo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.apps.mirobo.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
